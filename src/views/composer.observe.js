820492c3e117d79615ea0b776d6061a4447b952f
Christopher Blum <christopher.blum@xing.com>
2012-04-19 22:56:35 +0200

    Fix #46 where ALT+I causes problems on windows

--- src/views/composer.observe.js
+++ src/views/composer.observe.js
@@ -136,7 +136,7 @@
     dom.observe(element, "keydown", function(event) {
       var keyCode  = event.keyCode,
           command  = shortcuts[keyCode];
-      if ((event.ctrlKey || event.metaKey) && command) {
+      if ((event.ctrlKey || event.metaKey) && !event.altKey && command) {
         that.commands.exec(command);
         event.preventDefault();
       }
