66bcc4c7f627842b15e1cc6578e87038a0792e13
tiff <christopher.blum@spellboy.com>
2012-06-15 00:56:47 +0200

    Move caret to the end when textarea has attribute 'autofocus' (fixes #100)

--- src/views/composer.js
+++ src/views/composer.js
@@ -177,7 +177,7 @@
 
       // Simulate html5 autofocus on contentEditable element
       if (this.textarea.element.hasAttribute("autofocus") || document.querySelector(":focus") == this.textarea.element) {
-        setTimeout(function() { that.focus(); }, 100);
+        setTimeout(function() { that.focus(true); }, 100);
       }
 
       wysihtml5.quirks.insertLineBreakOnReturn(this);
