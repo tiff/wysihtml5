3573134da3873810e3341afd8039720bcda04612
tiff <christopher.blum@spellboy.com>
2012-08-17 18:15:28 +0200

    Android 4+ support (fixes #168)

--- test/browser_test.js
+++ test/browser_test.js
@@ -3,7 +3,8 @@ module("wysihtml5.browser", {
     iPad_iOS3:    "Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B334b Safari/531.21.10",
     iPhone_iOS3:  "Mozilla/5.0 (iPhone; U; CPU like Mac OS X; en) AppleWebKit/420+ (KHTML, like Gecko) Version/3.0 Mobile/1A543a Safari/419.3",
     iPad_iOS5:    "Mozilla/5.0 (iPad; CPU OS 5_0 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9A334 Safari/7534.48.3",
-    Android:      "Mozilla/5.0 (Linux; U; Android 2.1; en-us; Nexus One Build/ERD62) AppleWebKit/530.17 (KHTML, like Gecko) Version/4.0 Mobile Safari/530.17",
+    Android2:      "Mozilla/5.0 (Linux; U; Android 2.1; en-us; Nexus One Build/ERD62) AppleWebKit/530.17 (KHTML, like Gecko) Version/4.0 Mobile Safari/530.17",
+    Android4:      "Mozilla/5.0 (Linux; U; Android 4.0.4; en-gb; GT-I9300 Build/IMM76D) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30",
     Chrome:       "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_5_8; en-US) AppleWebKit/534.7 (KHTML, like Gecko) Chrome/7.0.517.44 Safari/534.7",
     OperaMobile:  "Opera/9.80 (S60; SymbOS; Opera Mobi/498; U; en-GB) Presto/2.4.18 Version/10.00",
     IE6:          "Mozilla/4.0 (Compatible; Windows NT 5.1; MSIE 6.0) (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
@@ -40,8 +41,11 @@ test("Check mobile contentEditable support", function() {
   wysihtml5.browser.USER_AGENT = this.userAgents.iPad_iOS5;
   ok(wysihtml5.browser.supported(), "iOS 5 is correctly supported");
   
-  wysihtml5.browser.USER_AGENT = this.userAgents.Android;
-  ok(!wysihtml5.browser.supported(), "Android is correctly unsupported");
+  wysihtml5.browser.USER_AGENT = this.userAgents.Android2;
+  ok(!wysihtml5.browser.supported(), "Android 2 is correctly unsupported");
+  
+  wysihtml5.browser.USER_AGENT = this.userAgents.Android4;
+  ok(!wysihtml5.browser.supported(), "Android 4 is correctly supported");
   
   wysihtml5.browser.USER_AGENT = this.userAgents.OperaMobile;
   ok(!wysihtml5.browser.supported(), "Opera Mobile is correctly unsupported");
